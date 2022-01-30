7c84187d7ee36a5f934cb3a3347ba7758d480545
george1605 <32997165+george1605@users.noreply.github.com>
2022-01-30 12:43:59 +0200

    replaced xchgax with achg

--- arm.h
+++ arm.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "lib.c"
 #define is_arm() sizeof(long) == 4 && sizeof(int) == 4
+#define achg(reg1,reg2) asm("xchg "reg1", "reg2)
+
+int* armbrk = 0x2A0000;
+
+void* armalloc(int bytes)
+{
+    void* u = armbrk;
+    u += (bytes + 1);
+
+}
 
 void xrqinstall(size_t ndx, void* addr)
 {
@@ -18,20 +28,4 @@ size_t cpsrget(){
 void cpsrset(size_t r)
 {
     asm("msr cpsr, %[ps]" : : [ps]"r" (r));
-}
-
-void xchgax(){
-    asm("xchg ax,bx");
-}
-
-void mulr(){
-    asm("mul R1,R2,R3"); 
-}
-
-void xchgbx(){
-    asm("xchg bx,cx");
-}
-
-void xchcx(){
-    asm("xchg cx,dx");   
 }
